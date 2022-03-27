600770c52249e82a65ea0466e7e77824ac185c38
v_xjhxi <v_xjhxi@tencent.com>
2022-03-27 20:17:39 +0800

    任务系统

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <el-input class="kuan" v-model="input" placeholder="请输入内容"></el-input>
+    <el-button type="primary" @click="tianjia">添加</el-button>
+    <div>
+      <div v-for="item in list" :key="item.id">
+        {{item.id+':'+item.value}},
+        <el-button type="danger" icon="el-icon-delete" circle size='mini' @click="shanchu(item.id)"></el-button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    data() {
+    return {
+      input: '',
+      list:[],
+    }
+   },
+   methods: {
+     tianjia(){
+       if(this.input==''){
+         this.$message.error('错了哦，这是一条错误消息');
+         return
+      }
+      let scx = true;
+      for(let i = 0;i<this.list.length;i++){
+        if(this.list[i].value==this.input){
+          scx=false
+        }
+      }
+      if(scx==false){
+        this.$message.error('错了哦，这是一条重复消息');
+        return
+      }
+       this.list.push({id:(Math.random()*10*100).toFixed(0),
+      value:this.input,
+     });
+     this.input="";
+     },
+     shanchu(v){
+       this.list=this.list.filter((item)=>item.id!=v)
+     },
+     
+    },
+    
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.kuan{
+  width: 500px;
 }
 </style>
